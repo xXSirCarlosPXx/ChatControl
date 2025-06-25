ebcf14e2f4a36a5e17bcae9c60ffad537e0b6e9b
devpedrobarbosa <devpedrobarbosa@gmail.com>
2025-06-25 05:10:41 -0300

    Feature: channels now support all faction relations: ally, truce and enemy.

--- chatcontrol-bukkit/src/main/java/org/mineacademy/chatcontrol/api/Party.java
+++ chatcontrol-bukkit/src/main/java/org/mineacademy/chatcontrol/api/Party.java
@@ -36,10 +36,33 @@ public abstract class Party {
 		};
 	};
 
+	/**
+	 * This is a channel shown only to players in the Factions allied to the sender's.
+	 */
 	public static final Party FACTION_ALLY = new Party("factions-ally") {
 		@Override
 		public boolean isInParty(Player receiver, Player sender) {
-			return HookManager.getOnlineFactionPlayers(sender).contains(receiver) || HookManager.getAlliedFactionPlayers(sender).contains(receiver);
+			return HookManager.getOnlineFactionPlayers(sender).contains(receiver) || HookManager.getRelatedFactionPlayers(sender, "ALLY").contains(receiver);
+		}
+	};
+
+	/**
+	 * This is a channel shown only to players in the Factions enemy to the sender's.
+	 */
+	public static final Party FACTION_ENEMY = new Party("factions-enemy") {
+		@Override
+		public boolean isInParty(Player receiver, Player sender) {
+			return HookManager.getOnlineFactionPlayers(sender).contains(receiver) || HookManager.getRelatedFactionPlayers(sender, "ENEMY").contains(receiver);
+		}
+	};
+
+	/**
+	 * This is a channel shown only to players in the Factions in truce with the sender's.
+	 */
+	public static final Party FACTION_TRUCE = new Party("factions-truce") {
+		@Override
+		public boolean isInParty(Player receiver, Player sender) {
+			return HookManager.getOnlineFactionPlayers(sender).contains(receiver) || HookManager.getRelatedFactionPlayers(sender, "TRUCE").contains(receiver);
 		}
 	};
 
